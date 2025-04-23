e90165c81a30c62a03e12d6f23f92d27eb1c38e3
Pedro Lucas <pedro.wo@live.com>
2025-04-23 17:30:05 -0400

    correção nos estilos

--- frontend/app/lista/page.tsx
+++ frontend/app/lista/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Lista from "@/src/components/ListaUsuario/Lista";
+import ProtectedRoute from "@/src/components/RotaProtegida/RotaProtegida";
 
 export default function PageLista() {
     return (
-
+        <ProtectedRoute>
         <Lista/>
-
+        </ProtectedRoute>
     )
 }
