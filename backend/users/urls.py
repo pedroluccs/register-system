2018ab099ba2e16b1299de623b2de9968cd0e726
Pedro Lucas <pedro.wo@live.com>
2025-04-23 15:23:28 -0400

    mudança no debug para testes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UsuarioViewSet, criar_usuario
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet)
@@ -8,4 +9,6 @@ router.register(r'usuarios', UsuarioViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('cadastro/', criar_usuario, name="cadastro_usuario"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
