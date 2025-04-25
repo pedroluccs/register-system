7e50b905676838abefd3b43190ebe7337b2a71f9
Pedro Lucas <pedro.wo@live.com>
2025-04-25 16:39:01 -0400

    alteraçao no css do projeto

--- frontend/src/components/Header/style.ts
+++ frontend/src/components/Header/style.ts
@@ -3,13 +3,25 @@ import styled from "styled-components";
 export const MainHeader = styled.header`
 margin: 12px;
 font-size: 20px;
-border-radius: 8px;
-border: 1px solid #fff;
 `
 
 export const Nav = styled.nav`
-margin: 10px;
+margin: 24px;
+margin-bottom: 36px;
 display: flex;
 justify-content: space-around;
 list-style-type: none;
+
+li {
+border: 2px solid transparent;
+border-radius: 8px;
+padding: 4px 8px;
+transition: border-color 0.5s;
+
+&:hover {
+border-radius: 8px;
+border: 2px solid #fff;
+padding: 4px 8px;
+}
+}
 `
--- frontend/src/components/Login/style.ts
+++ frontend/src/components/Login/style.ts
@@ -1,18 +1,41 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-margin-top: 15vh;
+margin: 0 auto;
+margin-top: 10vh;
+height: 600px;
+width: 400px;
 display: flex;
 flex-direction: column;
 align-items: center;
-height: 100vh;
+background: linear-gradient(400deg, black, white 500%);
+border: 1px solid rgba(255, 255, 255, 0.3);
+border-radius: 24px;
+
+h1 {
+font-size: 24px;
+margin-bottom: 36px;
+margin-top: 140px;
+}
 `
 export const Input = styled.input`
 margin: 10px;
-padding: 10px;
+padding: 12px;
+padding-right: 120px;
+border-radius: 8px;
+border: none;
+background-color: #383838;
+text-align: left;
 `
 export const Button = styled.button`
 margin: 10px;
-padding: 10px;
+padding: 12px 130px;
 cursor: pointer;
+background-color: #1b2d49;
+border-radius: 8px;
+border: none;
+
+&:hover {
+background-color:rgb(18, 53, 90);
+}
 `
