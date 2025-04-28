eed7d253e343fde940e6306469ac9e12bbd8d4d2
Pedro Lucas <pedro.wo@live.com>
2025-04-28 17:31:33 -0400

    feat: componente de proteção de rota com autenticação

--- frontend/app/cadastro/page.tsx
+++ frontend/app/cadastro/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-// import ProtectedRoute from "@/src/components/RotaProtegida/RotaProtegida";
+import ProtectedRoute from "@/src/components/RotaProtegida/RotaProtegida";
 import Cadastro from "../../src/components/CadastroUsuario/Cadastro";
 
 export default function PageCadastro() {
 return (
-   
+    <ProtectedRoute>
     <Cadastro />
-    
+     </ProtectedRoute>
 )
 }
